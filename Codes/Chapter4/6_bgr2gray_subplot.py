b24d3288a47d450a75e28cbb46d85b532d75ac2f
OdiSeU <secret36565@gmail.com>
2020-10-09 23:05:44 +0900

    201009 udpate

--- Codes/Chapter4/6_bgr2gray_subplot.py
+++ Codes/Chapter4/6_bgr2gray_subplot.py
@@ -1,29 +1,24 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_name = font_manager.FontProperties(fname="C:/Windows/Fonts/gulim.ttc").get_name()
+rc('font', family=font_name)
 
 img = cv2.imread('../../Images/tonkatsu.jpg')
 img2 = img.astype(np.uint16)
-b, g, r = cv2.split(img)
+b, g, r = cv2.split(img2)
 gray1 = ((b + g + r)/3).astype(np.uint8)
 gray2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-imgs = {'original': img, 'gray - 평균': gray1, 'gray - cvtColor 함수': gray2}
-plt.subplot(1, 3, 1)
-plt.title('original')
-plt.imshow(img[:,:,::-1])
-plt.xticks([]); plt.yticks([])
+imgs = {'original': img[:,:,::-1], 'gray - 평균': gray1, 'gray - cvtColor 함수': gray2}
 
-plt.subplot(1, 3, 2)
-plt.title('gray - 평균')
-plt.imshow(gray1)
-plt.xticks([]); plt.yticks([])
-
-plt.subplot(1, 3, 3)
-plt.title('gray - cvtColor 함수')
-plt.imshow(gray2)
-plt.xticks([]); plt.yticks([])
-
+for i, (key, value) in enumerate(imgs.items()):
+    plt.subplot(1, 3, i+1)
+    plt.title(key)
+    #cmap : value가 RGB(A)일 경우 무시
+    plt.imshow(value, cmap='gray')
+    plt.xticks([]); plt.yticks([])
 plt.show()
 
 cv2.waitKey(0)
